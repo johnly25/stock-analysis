fad0346d72d11a7a90565471e61867830cdb4fc4
johnny <94998830+johnly25@users.noreply.github.com>
2024-02-05 10:55:23 -0800

    delete main code

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,6 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        StockAnalyst stockAnalyst = new StockAnalyst();
-        String urlText = stockAnalyst.getUrlText();
-        List<String> categories = stockAnalyst.getStocksListCategories(urlText);
-        Map<String, String> subcategories = stockAnalyst.getStocksListsInListCategory(urlText, categories.get(1));
-        Map.Entry<String, String> entry = subcategories.entrySet().iterator().next();
-        String key = entry.getKey();
-        String value = entry.getValue();
-        System.out.println(key + ": " + value);
-        urlText = stockAnalyst.getUrlText("https://stockanalysis.com/list/ai-stocks/");
-        Map<Double, List<String>> percentChange = stockAnalyst.getTopCompaniesByChangeRate(urlText);
-        System.out.println(percentChange);
 
     }
 
